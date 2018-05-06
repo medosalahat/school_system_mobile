b8ba645daae23c5b65ec744f26c3cba3659f792d
mohammad M salahat <msalahat88@gmail.com>
2018-05-06 06:49:09 +0300

    change

--- controllers/Quiz_questions_answersController.php
+++ controllers/Quiz_questions_answersController.php
@@ -7,10 +7,65 @@
  */
 namespace app\controllers;
 use yii\rest\ActiveController;
+use yii;
 class Quiz_questions_answersController extends ActiveController{
     public $modelClass = 'app\models\QuizQuestionsAnswers';
     public $serializer = [
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
+
+    public function actions() {
+        $actions = parent::actions();
+        unset($actions[ 'index']);
+        return $actions;
+    }
+    public $prepareDataProvider;
+    public $dataFilter;
+
+    public function actionIndex() {
+        $requestParams = Yii::$app->getRequest()->getBodyParams();
+        if (empty($requestParams)) {
+            $requestParams = Yii::$app->getRequest()->getQueryParams();
+        }
+
+        $filter = null;
+        if ($this->dataFilter !== null) {
+            $this->dataFilter = Yii::createObject($this->dataFilter);
+            if ($this->dataFilter->load($requestParams)) {
+                $filter = $this->dataFilter->build();
+                if ($filter === false) {
+                    return $this->dataFilter;
+                }
+            }
+        }
+
+        if ($this->prepareDataProvider !== null) {
+            return call_user_func($this->prepareDataProvider, $this, $filter);
+        }
+
+        /* @var $modelClass \yii\db\BaseActiveRecord */
+        $modelClass = $this->modelClass;
+
+        $query = $modelClass::find();
+
+        if (!empty($filter)) {
+            $query->andWhere($filter);
+        }
+        $query->with(['quizQuestions','quizStudentsAnswers']);
+
+
+
+
+        return Yii::createObject([
+            'class' => yii\data\ActiveDataProvider::className(),
+            'query' => $query,
+            'pagination' => [
+                'params' => $requestParams,
+            ],
+            'sort' => [
+                'params' => $requestParams,
+            ],
+        ]);
+    }
 }
--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -1,10 +1,66 @@
 <?php
 namespace app\controllers;
 use yii\rest\ActiveController;
+use yii;
 class UsersController extends ActiveController{
     public $modelClass = 'app\models\User';
     public $serializer = [
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
+
+
+    public function actions() {
+        $actions = parent::actions();
+        unset($actions[ 'index']);
+        return $actions;
+    }
+    public $prepareDataProvider;
+    public $dataFilter;
+
+    public function actionIndex() {
+        $requestParams = Yii::$app->getRequest()->getBodyParams();
+        if (empty($requestParams)) {
+            $requestParams = Yii::$app->getRequest()->getQueryParams();
+        }
+
+        $filter = null;
+        if ($this->dataFilter !== null) {
+            $this->dataFilter = Yii::createObject($this->dataFilter);
+            if ($this->dataFilter->load($requestParams)) {
+                $filter = $this->dataFilter->build();
+                if ($filter === false) {
+                    return $this->dataFilter;
+                }
+            }
+        }
+
+        if ($this->prepareDataProvider !== null) {
+            return call_user_func($this->prepareDataProvider, $this, $filter);
+        }
+
+        /* @var $modelClass \yii\db\BaseActiveRecord */
+        $modelClass = $this->modelClass;
+
+        $query = $modelClass::find();
+
+        if (!empty($filter)) {
+            $query->andWhere($filter);
+        }
+        $query->with(['chats','chats0','coursesDivisions','notes','notes0','studentCoursesDivisions','studentFamilies','studentFamilies0','userType']);
+
+
+
+
+        return Yii::createObject([
+            'class' => yii\data\ActiveDataProvider::className(),
+            'query' => $query,
+            'pagination' => [
+                'params' => $requestParams,
+            ],
+            'sort' => [
+                'params' => $requestParams,
+            ],
+        ]);
+    }
 }
